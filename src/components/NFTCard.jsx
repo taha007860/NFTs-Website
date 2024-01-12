9aa6c020e1db5e2a0dcb193df12dabb8d45d6838
Mohammad Zaatari <mohamadalzaatari748@gmail.com>
2024-01-12 08:20:02 +0200

    css

--- src/components/NFTCard.jsx
+++ src/components/NFTCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "../styles/NFTCardVideo.css";
+import "../styles/NFTCard.css";
 import thumbs from '../assets/images/thumbs.png'
 import heart from '../assets/images/love.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
